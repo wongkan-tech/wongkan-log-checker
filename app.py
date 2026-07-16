a02c7fd0d34a39e39e6599140592b7b40fe1dc2f
wongkan-tech <TH9storm@gmail.com>
2026-07-16 23:16:40 +0700

    Update app.py

--- app.py
+++ app.py
@@ -3886,11 +3886,11 @@ client = OpenAI(
 if "messages" not in st.session_state:
     st.session_state.messages = []
 
-        for message in st.session_state.messages:
-            with st.chat_message(message["role"]):
-                st.markdown(message["content"])
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+                	st.markdown(message["content"])
 
-        if prompt := st.chat_input("พิมพ์รหัส Error หรือวางข้อความ Log ให้ AI ช่วยวิเคราะห์ที่นี่..."):
+if prompt := st.chat_input("พิมพ์รหัส Error หรือวางข้อความ Log ให้ AI ช่วยวิเคราะห์ที่นี่..."):
             st.session_state.messages.append({"role": "user", "content": prompt})
             with st.chat_message("user"):
                 st.markdown(prompt)
